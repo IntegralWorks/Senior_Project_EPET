ecb6251ba322e74618f361a247813d0b367e88f4
Javi Salazar <javisalazar2@gmail.com>
2019-03-22 11:46:42 -0500

    Backend

---
+++ dev/backend.ino
@@ -0,0 +1,345 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <WebSocketsServer.h>
+
+ESP8266WiFiMulti wifiMulti;       // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+ESP8266WebServer server(80);       // Create a webserver object that listens for HTTP request on port 80
+WebSocketsServer webSocket(81);    // create a websocket server on port 81
+
+File fsUploadFile;                 // a File variable to temporarily store the received file
+
+const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
+const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
+
+const char *OTAName = "ESP8266";           // A name and a password for the OTA service
+const char *OTAPassword = "esp8266";
+
+// String page = "";
+// int TRIG = D0;
+// int ECHO = D1;
+// int GPIO_0 = D2;
+// int GPIO_1 = D7;
+
+#define TRIG D0
+#define ECHO D1
+#define GPIO_0 D2
+#define GPIO_1 D7
+
+bool GPIO_0_status = false;
+bool GPIO_1_status = false;
+bool water_sensor_status = false;
+double water_container_0_level = 0;
+double water_container_1_level = 0;
+
+long hp = 0; //sorry for the messy global
+
+
+void sonic_sensor_setup(){
+  Serial.begin(115200);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  Serial.print(hp);
+  Serial.print("\n");
+  delay(1000);
+  
+}
+
+int sonic_sensor_loop(){
+      
+// Sending to computer
+//  Serial.print("Current water level is: ");
+  long t = 0, h = 0;
+  //hp = 0;
+
+  
+  // Transmitting pulse
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  
+  // Waiting for pulse
+  t = pulseIn(ECHO, HIGH);
+  
+  // Calculating distance 
+  h = t / 58;
+ 
+  h = h - 6;  // offset correction
+  h = 50 - h;  // water height, 0 - 50 cm
+  
+  hp = 2 * h;  // distance in %, 0-100 %
+  return hp;
+  // Serial.print(" cm\n");
+  // Serial.print("\n");
+  
+  delay(1000);
+}
+
+const char* mdnsName = "esp8266"; // Domain name for the mDNS responder
+
+void setup() {
+  sonic_sensor_setup();
+
+  // pinMode(LED_RED, OUTPUT);    // the pins with LEDs connected are outputs
+  // pinMode(LED_GREEN, OUTPUT);
+  // pinMode(LED_BLUE, OUTPUT);
+
+  Serial.begin(115200);        // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println("\r\n");
+
+  startWiFi();                 // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+  //check!
+  
+  startOTA();                  // Start the OTA service
+  
+  startSPIFFS();               // Start the SPIFFS and list all contents
+
+  startWebSocket();            // Start a WebSocket server
+  
+  startMDNS();                 // Start the mDNS responder
+
+  startServer();               // Start a HTTP server with a file read handler and an upload handler
+  
+}
+
+
+
+bool rainbow = false;             // The rainbow effect is turned off on startup
+
+unsigned long prevMillis = millis();
+int hue = 0;
+
+void loop() {
+  sonic_sensor_loop();
+  webSocket.loop();                           // constantly check for websocket events
+  server.handleClient();                      // run the server
+  ArduinoOTA.handle();                        // listen for OTA events
+
+}
+
+
+
+void startWiFi() { // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+  WiFi.softAP(ssid, password);             // Start the access point
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started\r\n");
+
+  wifiMulti.addAP("ssid_from_AP_1", "your_password_for_AP_1");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting");
+  while (wifiMulti.run() != WL_CONNECTED && WiFi.softAPgetStationNum() < 1) {  // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  if(WiFi.softAPgetStationNum() == 0) {      // If the ESP is connected to an AP
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());             // Tell us what network we're connected to
+    Serial.print("IP address:\t");
+    Serial.print(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
+  } else {                                   // If a station is connected to the ESP SoftAP
+    Serial.print("Station connected to ESP8266 AP");
+  }
+  Serial.println("\r\n");
+}
+
+void startOTA() { // Start the OTA service
+  ArduinoOTA.setHostname(OTAName);
+  ArduinoOTA.setPassword(OTAPassword);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+    // digitalWrite(LED_RED, 0);    // turn off the LEDs
+    // digitalWrite(LED_GREEN, 0);
+    // digitalWrite(LED_BLUE, 0);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\r\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready\r\n");
+}
+
+void startSPIFFS() { // Start the SPIFFS and list all contents
+  SPIFFS.begin();                             // Start the SPI Flash File System (SPIFFS)
+  Serial.println("SPIFFS started. Contents:");
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {                      // List the file system contents
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("\tFS File: %s, size: %s\r\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    Serial.printf("\n");
+  }
+}
+
+void startWebSocket() { // Start a WebSocket server
+  webSocket.begin();                          // start the websocket server
+  webSocket.onEvent(webSocketEvent);          // if there's an incomming websocket message, go to function 'webSocketEvent'
+  Serial.println("WebSocket server started.");
+}
+
+void startMDNS() { // Start the mDNS responder
+  MDNS.begin(mdnsName);                        // start the multicast domain name server
+  Serial.print("mDNS responder started: http://");
+  Serial.print(mdnsName);
+  Serial.println(".local");
+}
+
+void startServer() { // Start a HTTP server with a file read handler and an upload handler
+  server.on("/edit.html",  HTTP_POST, []() {  // If a POST request is sent to the /edit.html address,
+    server.send(200, "text/plain", ""); 
+  }, handleFileUpload);                       // go to 'handleFileUpload'
+
+  server.onNotFound(handleNotFound);          // if someone requests any other file or page, go to function 'handleNotFound'
+                                              // and check if the file exists
+
+  server.begin();                             // start the HTTP server
+  Serial.println("HTTP server started.");
+}
+
+void handleNotFound(){ // if the requested file or page doesn't exist, return a 404 not found error
+  if(!handleFileRead(server.uri())){          // check if the file exists in the flash memory (SPIFFS), if so, send it
+    server.send(404, "text/plain", "404: File Not Found");
+  }
+}
+
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
+  String contentType = getContentType(path);             // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) { // If the file exists, either as a compressed archive, or normal
+    if (SPIFFS.exists(pathWithGz))                         // If there's a compressed version available
+      path += ".gz";                                         // Use the compressed verion
+    File file = SPIFFS.open(path, "r");                    // Open the file
+    size_t sent = server.streamFile(file, contentType);    // Send it to the client
+    file.close();                                          // Close the file again
+    Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
+  return false;
+}
+
+void handleFileUpload(){ // upload a new file to the SPIFFS
+  HTTPUpload& upload = server.upload();
+  String path;
+  if(upload.status == UPLOAD_FILE_START){
+    path = upload.filename;
+    if(!path.startsWith("/")) path = "/"+path;
+    if(!path.endsWith(".gz")) {                          // The file server always prefers a compressed version of a file 
+      String pathWithGz = path+".gz";                    // So if an uploaded file is not compressed, the existing compressed
+      if(SPIFFS.exists(pathWithGz))                      // version of that file must be deleted (if it exists)
+         SPIFFS.remove(pathWithGz);
+    }
+    Serial.print("handleFileUpload Name: "); Serial.println(path);
+    fsUploadFile = SPIFFS.open(path, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
+    path = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize); // Write the received bytes to the file
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile) {                                    // If the file was successfully created
+      fsUploadFile.close();                               // Close the file again
+      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      server.sendHeader("Location","/success.html");      // Redirect the client to the success page
+      server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
+  }
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) { // When a WebSocket message is received
+  switch (type) {
+    case WStype_DISCONNECTED:             // if the websocket is disconnected
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {              // if a new websocket connection is established
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+      }
+      break;
+    case WStype_TEXT:                     // if new text data is received
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+
+      if (payload[0] == 'defineWaterLevel') {
+      }
+
+      if (payload[0] == 'getWaterLevel') {
+      }
+
+      if (payload[0] == 'toggleGPIO_0_off') {
+      
+        bool gpio0status = false;
+        digitalWrite(GPIO_0, LOW);
+        
+      }
+
+      if (payload[0] == 'toggleGPIO_0_on') {
+      
+        bool gpio0status = true;.
+        digitalWrite(GPIO_0, HIGH);
+
+      }
+
+      if (payload[0] == 'toggleGPIO_1_off') {
+      
+        bool gpio1status = false;
+        digitalWrite(GPIO_1, LOW);
+
+      }
+
+      if (payload[0] == 'toggleGPIO_1_on') {
+      
+        bool gpio1status = true;
+        digitalWrite(GPIO_1, HIGH);
+
+      }
+
+      break;
+  }
+}
+
+String formatBytes(size_t bytes) { // convert sizes in bytes to KB and MB
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  }
+}
+
+String getContentType(String filename) { // determine the filetype of a given filename, based on the extension
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
